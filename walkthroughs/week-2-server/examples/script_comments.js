d72c47143f57fd21e7a7547cc489d65bd9b419bd
Ishika Kumar <ishika.2017.114@gmail.com>
2020-07-10 23:16:07 +0530

    Using data store

--- walkthroughs/week-2-server/examples/script_comments.js
+++ walkthroughs/week-2-server/examples/script_comments.js
@@ -12,25 +12,41 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Fetches stats from the servers and adds them to the DOM.
- */
-function loadConvo() {
-    fetch('/Fetch-Conversations').then(response => response.json()).then((stats) => {
-      // stats is an object, not a string, so we have to
-      // reference its fields to create HTML content
-      const elem= document.getElementById('Fetch-Conversations-container');
-      statsListElement.innerHTML = '';
-      for(let i=0;i< convo.length(); ++i){
-        elem.appendChild(createListElement(convo[i]));
-      }
-    });
-  }
-  
-  /** Creates an <li> element containing text. */
-  function createListElement(text) {
-    const liElement = document.createElement('li');
-    liElement.innerText = text;
-    return liElement;
-  }
-  
+/** Fetches tasks from the server and adds them to the DOM. */
+function loadTasks() {
+  fetch('/list-tasks').then(response => response.json()).then((tasks) => {
+    const taskListElement = document.getElementById('task-list');
+    tasks.forEach((task) => {
+      taskListElement.appendChild(createTaskElement(task));
+    })
+  });
+}
+
+/** Creates an element that represents a task, including its delete button. */
+function createTaskElement(task) {
+  const taskElement = document.createElement('li');
+  taskElement.className = 'task';
+
+  const titleElement = document.createElement('span');
+  titleElement.innerText = task.title;
+
+  const deleteButtonElement = document.createElement('button');
+  deleteButtonElement.innerText = 'Delete';
+  deleteButtonElement.addEventListener('click', () => {
+    deleteTask(task);
+
+    // Remove the task from the DOM.
+    taskElement.remove();
+  });
+
+  taskElement.appendChild(titleElement);
+  taskElement.appendChild(deleteButtonElement);
+  return taskElement;
+}
+
+/** Tells the server to delete the task. */
+function deleteTask(task) {
+  const params = new URLSearchParams();
+  params.append('id', task.id);
+  fetch('/delete-task', {method: 'POST', body: params});
+}
