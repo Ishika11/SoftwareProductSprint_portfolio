edf2adec99f0e241d68732440172077474a14393
Ishika Kumar <ishika.2017.114@gmail.com>
2020-07-22 18:38:15 +0530

    map

--- portfolio/src/main/webapp/js/script.js
+++ portfolio/src/main/webapp/js/script.js
@@ -100,3 +100,8 @@ function createMap() {
     document.getElementById('map'),
     { center: { lat: 37.422, lng: -122.084 }, zoom: 16 });
 }
+function createMap() {
+  const map = new google.maps.Map(
+    document.getElementById('map'),
+    { center: { lat: 37.422, lng: -122.084 }, zoom: 16 });
+}
